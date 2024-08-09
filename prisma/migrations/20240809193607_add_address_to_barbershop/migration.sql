aad21c8c0b478a084f04cd71fa15f3a78c1ef714
souzzagab <gabriel.souza@lacreisaude.com.br>
2024-08-09 17:59:08 -0300

    chore: add prettier

---
+++ prisma/migrations/20240809193607_add_address_to_barbershop/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `adress` on the `Barbershop` table. All the data in the column will be lost.
+  - You are about to drop the column `email` on the `Barbershop` table. All the data in the column will be lost.
+  - Added the required column `address` to the `Barbershop` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Barbershop" DROP COLUMN "adress",
+DROP COLUMN "email",
+ADD COLUMN     "address" TEXT NOT NULL;
