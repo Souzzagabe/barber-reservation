d800d6dcf85aa971c498ab5fa3d857f2be2f960c
souzzagab <gabriel.souza@lacreisaude.com.br>
2024-08-13 12:26:53 -0300

    feat: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import { SearchIcon } from "lucide-react"
 import Header from "./_components/Header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { SearchIcon } from "lucide-react"
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
 
 export default function Home() {
   return (
@@ -26,6 +29,28 @@ export default function Home() {
             alt="Agende com os melhores"
           />
         </div>
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage
+                    className="rounded-full"
+                    src="https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png"
+                  />
+                </Avatar>
+                <p className="text-sm">Barbearia GS</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
